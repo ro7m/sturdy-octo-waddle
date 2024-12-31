742f2ae10ad0ae18fe8f1058a8489477ce17177e
ro7m <robin.mundra@gmail.com>
2024-12-31 06:47:39 -0800

    add steps to build the app

--- python/setup.py
+++ python/setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flutter_onnxtr_ffi',
+    name='flutter_onnx_ffi',
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.21.0',
         'onnxruntime>=1.8.0',
         'Pillow>=8.0.0',
-        'onnxtr @ git+https://github.com/felixdittrich92/OnnxTR.git'
+        'onnxtr>=0.6.0'
     ],
     python_requires='>=3.7',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='Python bridge for Flutter OnnxTR FFI integration',
-    long_description=open('README.md').read(),
+    author='ro7m',
+    author_email='',
+    description='Python bridge for Flutter DocTR FFI integration',
+    long_description='Python bridge for flutter FFI ',
     long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 3 - Alpha',
